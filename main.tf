cae07d4a8e64e1cbae375c85fd1374518547904d
Arpakathy <108733076+Arpakathy@users.noreply.github.com>
2024-03-25 09:55:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ locals {
 }
 data "external" "get_mime" {
   for_each = local.website_files
-  program  = ["/bin/sh", "-c", "./mime.sh"]
+  program  = ["/bin/sh", "-c", "sudo chmod +x mime.sh" , ./mime.sh"]
   query = {
     filepath : "${var.cp-path}/${each.key}"
   }
