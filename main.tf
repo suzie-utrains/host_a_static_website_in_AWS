c2eb6c07c300ea029cc0166439579f67be34bc83
Arpakathy <kathyghomo@gmail.com>
2024-03-25 14:13:15 +0100

    Update file

--- main.tf
+++ main.tf
@@ -19,8 +19,34 @@ resource "aws_s3_bucket" "bucket1" {
 
   bucket = var.bucket_name
   force_destroy = true
-  acl = "private"
   
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~ Configure the bucket acl ~~~~~~~~~~~~~~~~~~~~~~
+resource "aws_s3_bucket_ownership_controls" "rule" {
+  bucket = aws_s3_bucket.bucket1.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access-bloc" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.rule,
+    aws_s3_bucket_public_access_block.access-bloc,
+  ]
+
+  bucket = aws_s3_bucket.bucket1.id
+  acl    = "public-read"
 }
 
 # ~~~~~~~~~~~~~~~~~ Upload the site content in the bucket ~~~~~~~~~~~~~
