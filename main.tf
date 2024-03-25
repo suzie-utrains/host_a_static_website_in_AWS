9bd0162646f6c54fcde61593c668dc28d89e95e1
Arpakathy <kathyghomo@gmail.com>
2024-03-25 09:59:43 +0100

    Upload main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ locals {
 }
 data "external" "get_mime" {
   for_each = local.website_files
-  program  = ["/bin/sh", "-c", "sudo chmod +x mime.sh" , ./mime.sh"]
+  program  = ["/bin/bash", "-c", "sudo chmod +x mimi.sh" , "./mime.sh"]
   query = {
     filepath : "${var.cp-path}/${each.key}"
   }
