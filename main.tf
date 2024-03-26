ee0d886cd3bb1cd6ee8c4d9384550dc039c6edac
Arpakathy <kathyghomo@gmail.com>
2024-03-26 05:19:24 +0100

    Remove Bucket Policy

--- main.tf
+++ main.tf
@@ -54,35 +54,31 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
 }
 
 # ~~~~~~~~~~~ Configure public access parameters in the bucket ~~~~~~~
-resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
+resource "aws_s3_bucket_ownership_controls" "rule" {
+
+  bucket = aws_s3_bucket.bucket1.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+}
+resource "aws_s3_bucket_public_access_block" "access-bloc" {
+
   bucket = aws_s3_bucket.bucket1.id
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+
 }
+resource "aws_s3_bucket_acl" "example" {
 
-# ~~~~~~~~~~~~~~~~~~~~~~ Create the bucket policy ~~~~~~~~~~~~~~~~~~~~~
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket1.id
+  acl    = "public-read"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.bucket1.arn}/*"
-        ]
-      }
-    ]
-  })
+  depends_on = [ aws_s3_bucket_ownership_controls.rule, aws_s3_bucket_public_access_block.access-bloc]
+
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~ Configure CloudFont ~~~~~~~~~~~~~~~~~~~~~
