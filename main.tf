74181cc531dcfd1d118638b2d08c846345be806d
Arpakathy <kathyghomo@gmail.com>
2024-03-26 06:44:21 +0100

    Remove Bucket Policy

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_cloudfront_distribution" "web-distribution" {
 
   price_class = "PriceClass_200"
 
-  depends_on = [ aws_s3_bucket.bucket1 , null_resource.upload_files, aws_s3_bucket_policy.bucket_policy ]
+  depends_on = [ aws_s3_bucket.bucket1 , null_resource.upload_files, aws_s3_bucket_acl.bucket1-acl ]
   
 }
 
