404c01b1507e8ffb4b4c0978f78408343a477d56
suzie-utrains <98763564+suzie-utrains@users.noreply.github.com>
2024-10-29 12:22:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
 resource "null_resource" "upload_files" {
 
   provisioner "local-exec"  {
-      command = "aws s3 sync ./${var.cp-path} s3://${aws_s3_bucket.bucket1.bucket}/ --debug" 
+      command = "aws s3 sync ./${var.cp-path} s3://${aws_s3_bucket.bucket1.bucket}/ --region ${var.region} --debug" 
 }
  
 depends_on = [aws_s3_bucket.bucket1 , aws_s3_bucket_policy.allow_access]
